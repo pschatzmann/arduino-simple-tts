2254c1aae582787a5d302bf0b12fbbab06dbd16b
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-02-24 18:10:41 +0100

    audio-analog avoid noise

--- examples/audio-analog/audio-analog.ino
+++ examples/audio-analog/audio-analog.ino
@@ -42,7 +42,10 @@ void increment() {
 
 void loop() {
     // speach output
+    out.begin();
     utt.say(number, "usd");
+    delay(200);
+    out.end(); // closing to avoid disturbing noise
 
     increment();
     delay(1000);
